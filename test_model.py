ca30408d9a1d984ea1abd4441741d6560881d84e
gsandy <gsandy@rhnube.vip>
2024-11-11 06:30:11 -0500

    Super Mega Me las pelas caunsad

--- test_model.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-import tensorflow_hub as hub
-
-# Cargar el modelo desde TensorFlow Hub
-model_url = 'https://tfhub.dev/google/faster_rcnn/openimages_v4/inception_resnet_v2/1'
-model = hub.load(model_url)
-
-# Imprimir las firmas disponibles
-print("Signatures:", model.signatures)
